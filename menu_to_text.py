97f2f147769af13ac892dca673152d854d3e0f27
GateauXD <dantrandev@gmail.com>
2020-04-04 01:27:27 -0700

    Found a potential solution to grey-scale

--- menu_to_text.py
+++ menu_to_text.py
@@ -23,10 +23,27 @@ for index in range(100):
 
     for word in text:
         try:
+            if len(word) <= 1:
+                continue
             if english_dict.check(word):
                 score += 1
         except:
             print("Something Went Wrong. Continue")
     score_tracker.append(score)
 
-print(score_tracker)
+# Find the index value of the max value
+best_value = score_tracker.index(max(score_tracker))
+
+# Convert the image to grayscale
+image = Image.open(image_path).convert('L')
+image = image.point(lambda x: 0 if x < (100 + best_value) else 250, '1')
+image.save(save_path)
+
+# Convert the image to txt
+custom_oem_psm_config = r'--oem 3 --psm 6'
+text = pytesseract.image_to_string(Image.open(save_path), config=custom_oem_psm_config).split(" ")
+
+# Save Output
+text_file = open("Output.txt", "w")
+text_file.write(text)
+text_file.close()
